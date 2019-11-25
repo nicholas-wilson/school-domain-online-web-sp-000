c85faf8a86816a486c5c4352ec029abdb3246041
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 20:53:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,6 +4,16 @@ class School
 
   def initialize(name)
     @name = name
-    @roster = {}
+    @roster = []
   end
+
+  def add_student(student, grade)
+    
+  end
+
+  def grade(grade_num)
+    students_in_grade = []
+    @roster.each do |student, grade|
+      if grade.to_i == grade_num.to_i
+        students_in_grade
 end
